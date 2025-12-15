3dcca1a566e29b41f0648a1429eb699a49cb805f
Alcino Cunha <alcino@di.uminho.pt>
2025-12-15 10:16:23 -0500

    Isolation example

---
+++ Examples/Isolation/isolation_spec.py
@@ -0,0 +1,27 @@
+import sys
+import itertools
+
+if __name__ == "__main__":
+    # check command line arguments
+    if len(sys.argv) != 4:
+        print("Usage: python isolation.py <transactions> <keys> <values>")
+        sys.exit(1)
+
+    T = int(sys.argv[1])  # number of transactions
+    K = int(sys.argv[2])  # number of keys
+    V = int(sys.argv[3])  # number of values
+
+    def same_transactions(A,B):
+        stmts = []
+        for t in range(T):
+            for k in range(K):
+                stmts.append(f"(reads_{t}_{k}[{A}] = reads_{t}_{k}[{B}] & writes_{t}_{k}[{A}] = writes_{t}_{k}[{B}])")
+        return "(" + " & ".join(stmts) + ")"
+    
+    def all_installed(A):
+        stmts = []
+        for t in range(T):
+            stmts.append(f"installed_{t}[{A}]")
+        return "F (" + " & ".join(stmts) + ")"
+    
+    print("Forall A . Exists B . !(" + all_installed("A") + " & (" + same_transactions("A","B") + " -> !(" + all_installed("B") + ")))" )

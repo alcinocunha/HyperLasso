5e21ea55dbf03ab74662c852eec149532f57957b
Alcino Cunha <alcino@di.uminho.pt>
2025-12-18 08:36:20 -0500

    bakery specs

---
+++ Examples/Bakery/example.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    # check command line arguments
+    if len(sys.argv) != 2:
+        print("Usage: python bakery.py <processes>")
+        sys.exit(1)
+
+    N = int(sys.argv[1])  # number of processes
+
+    print("Forall A . !(" + " & ".join([f"F (pc_{i}[A] = 4)" for i in range(N)]) + ")")
---
+++ Examples/Bakery/symmetric.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    # check command line arguments
+    if len(sys.argv) != 2:
+        print("Usage: python bakery.py <processes>")
+        sys.exit(1)
+
+    N = int(sys.argv[1])  # number of processes
+
+    print("Forall A . Exists B . G(" + " & ".join([f"pc_{i}[A] = pc_{(i+1) % N}[B]" for i in range(N)]) + ")")
